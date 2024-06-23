0c18f41428cacb8c50f0f67c608ade3959199b8d
nicopyds <nicopyds@gmail.com>
2024-06-23 20:00:14 +0200

    Analysed the data and started to implement sha256 to check if 2 photos are the same.

---
+++ config.py
@@ -0,0 +1,22 @@
+TOP_FILE_EXTENSIONS = {
+    '.jpg'    : "photo",
+    '.jpeg'   : "photo",
+    '.mp4'    : "video",
+    '.file'   : "na",
+    '.heic'   : "photo", # iPhone
+    '.mov'    : "video", # iPhone
+    '.opus'   : "audio",
+    '.m4v'    : "video", # iTunes
+    '.webp'   : "photo", # Google
+    '.png'    : "photo",
+    '.ipynb'  : "code",
+    '.csv'    : "data",
+    '.mp3'    : "audio",
+    '.pdf'    : "docs",
+    '.exo'    : "video", # Video Chunk from a streaming platform
+    '.dog3'   : "na", 
+    '.py'     : "code",
+    '.ldb'    : "data",  # Access
+    '.dog1'   : "na",
+    ".html"   : "code"
+}
